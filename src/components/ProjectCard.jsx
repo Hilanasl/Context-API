8913ca46af87de5ab43daa986ac4af0c9783289b
Hilana <hilana.sene@gmail.com>
2022-10-24 17:49:43 +0200

    link to projects added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ProjectCard = (props) => {
   return (
     <div className="ProjectCard">
-      <h3>{props.project.name}</h3>
-      <p>{props.project.technologies}</p>
+      <a href={props.project.website} target="_blank" rel="noreferrer">
+        <h3>{props.project.name}</h3>
+        <p>{props.project.technologies}</p>
+      </a>
     </div>
   );
 };
